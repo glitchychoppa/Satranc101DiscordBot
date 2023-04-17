a7ec3b4784eab6ddd5df899717a4429c31c48cd7
Omer Suat <omersuatdemir@gmail.com>
2023-04-17 14:37:01 +0300

    bildir, report update

--- commands/bildir.js
+++ commands/bildir.js
@@ -7,102 +7,154 @@ const MongoClient = require("mongodb").MongoClient;
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('bildir')
-        .setDescription('Hile olduğunu düşündüğünüz oyunları atabilirsiniz.')
-        .addStringOption(option => option
-            .setName('taraf')
-            .setDescription('Şüpheli taraf.')
-            .setRequired(true)
-            .setChoices(
-                { name: 'Beyaz', value: 'beyaz' },
-                { name: 'Siyah', value: 'siyah' }
-            ))
-        .addStringOption(option => option
-            .setName('id')
-            .setDescription('Oyun ID\'si.')
-            .setRequired(true))
-        .addStringOption(option => option
-            .setName('açıklama')
-            .setDescription('Hakemlere iletmek istediğin mesaj.')),
+        //.setDescription('Hile olduğunu düşündüğünüz oyunlari atabilirsiniz.')
+        .setDescription('hile')
+        .addSubcommand(subcommand => subcommand
+            .setName('lichess')
+            .setDescription('Lichess kullanıcısını bildir.')
+            .addStringOption(option => option
+                .setName('id')
+                .setDescription('Şüpheli hesabın kullanici adi.')
+                .setRequired(true))
+            .addStringOption(option => option
+                .setName('aciklama')
+                .setDescription('Oyunun linkini yapıştırın ve sorununuzu açıklayın.')
+                .setRequired(true)))
+        .addSubcommand(subcommand => subcommand
+            .setName('chess_com')
+            .setDescription('Chess.com kullanıcısını bildir.')
+            .addStringOption(option => option
+                .setName('id')
+                .setDescription('Süpheli hesabin kullanici adi.')
+                .setRequired(true))
+            .addStringOption(option => option
+                .setName('aciklama')
+                .setDescription('Oyunun linkini yapistirin ve sorununuzu aciklayin.')
+                .setRequired(true))),
     async execute(interaction, client) {
 
-        const config = {
-            headers: {
-                'Accept': 'application/json'
-            }
-        };
+        switch (interaction.options.getSubcommand()) {
+            case 'lichess':
 
-        const gameId = interaction.options.getString('id').slice(0, 8)
+                //lichess api ile kullanıcının lichess hesabının biyografi (açıklama) bölümüne erişiyoruz.
+                axios.get('https://lichess.org/api/user/' + interaction.options.getString('id').toLowerCase())
+                    .then(function (response) {
+                        const mongoClient = new MongoClient(dbConnectionString);
 
-        axios.get(`https://lichess.org/game/export/${gameId}`, config)
-            .then(response => {
+                        (async () => {
+                            try {
+                                const result = await mongoClient.db(mongoDB).collection(mongoCol).findOne({ lichessID: response.data.id })
 
-                const userName = interaction.options.getString('taraf') == 'beyaz'
-                    ? response.data.players.white.user.name : response.data.players.black.user.name;
+                                const embed = new EmbedBuilder()
+                                    .setColor(0x2cee1a)
+                                    .setTitle('Şikayetiniz Hakemlere İletildi')
+                                    .setURL('https://lichess.org/@/' + response.data.id)
+                                    .setFields(
+                                        { name: 'Kullanıcı', value: `<@${result.discordID}> - ${response.data.username}` },
+                                        { name: 'Hesap Linki', value: 'https://lichess.org/@/' + response.data.id },
+                                    )
+                                    .setThumbnail('https://cdn.discordapp.com/attachments/1065015635299537028/1066379362414379100/Satranc101Logo_1.png');
 
-                const userId = interaction.options.getString('taraf') == 'beyaz'
-                    ? response.data.players.white.user.id : response.data.players.black.user.id;
+                                const message = interaction.reply({
+                                    embeds: [embed],
+                                    ephemeral: true
+                                });
 
-                const url = `https://lichess.org/${gameId}`;
+                                await startSession(client, interaction.options.getSubcommand(), `<@${result.discordID}> - ${response.data.username}`, response.data.id, interaction.options.getString('aciklama'));
+                                console.log(`${interaction.user.tag} - ${interaction.user.id} bir oyun bildirdi.`)
 
-                const mongoClient = new MongoClient(dbConnectionString);
+                            } catch (error) {
+                                const embed = new EmbedBuilder()
+                                    .setColor(0x2cee1a)
+                                    .setTitle('Şikayetiniz Hakemlere İletildi')
+                                    .setURL('https://lichess.org/@/' + response.data.id)
+                                    .setFields(
+                                        { name: 'Kullanıcı', value: `${response.data.username}` },
+                                        { name: 'Hesap Linki', value: 'https://lichess.org/@/' + response.data.id },
+                                    )
+                                    .setThumbnail('https://cdn.discordapp.com/attachments/1065015635299537028/1066379362414379100/Satranc101Logo_1.png');
 
-                (async () => {
-                    try {
-                        const result = await mongoClient.db(mongoDB).collection(mongoCol).findOne({ lichessID: userId })
-    
-                        const embed = new EmbedBuilder()
-                            .setColor(0x2cee1a)
-                            .setTitle('Şikayetiniz Hakemlere İletildi')
-                            .setURL(url)
-                            .setFields(
-                                { name: 'Kullanıcı', value: `<@${result.discordID}> - ${userName}` },
-                                { name: 'Hesap Linki', value: 'https://lichess.org/@/' + userId },
-                                { name: 'Oyun Linki', value: url },
-                            )
+                                const message = interaction.reply({
+                                    embeds: [embed],
+                                    ephemeral: true
+                                });
+
+                                await startSession(client, interaction.options.getSubcommand(), response.data.username, response.data.id, interaction.options.getString('aciklama'));
+                                console.log(`${interaction.user.tag} - ${interaction.user.id} bir oyun bildirdi.`);
+                            }
+                        })();
+                    })
+                    .catch(error => {
+                        const invalidAccount = new EmbedBuilder()
+                            .setColor(0xec0505)
+                            .setTitle('Hesap Bulunamadı.')
+                            .setDescription(`\`${interaction.options.getString('id')}\` adında bir lichess.org hesabı bulunamadı.`)
                             .setThumbnail('https://cdn.discordapp.com/attachments/1065015635299537028/1066379362414379100/Satranc101Logo_1.png');
-    
-                        const message = interaction.reply({
-                            embeds: [embed],
-                            ephemeral: true
-                        });
-    
-                        await startSession(client,`<@${result.discordID}> - ${userName}`, userId, url, interaction.options.getString('açıklama'));
-                        console.log(`${interaction.user.tag} - ${interaction.user.id} bir oyun bildirdi.`)
-    
-                    } catch (error) {
-                        const embed = new EmbedBuilder()
-                            .setColor(0x2cee1a)
-                            .setTitle('Şikayetiniz Hakemlere İletildi')
-                            .setURL(url)
-                            .setFields(
-                                { name: 'Kullanıcı', value: `${userName}` },
-                                { name: 'Hesap Linki', value: 'https://lichess.org/@/' + userId },
-                                { name: 'Oyun Linki', value: url },
-                            )
+
+                        interaction.reply({ embeds: [invalidAccount], ephemeral: true });
+                    });
+                break;
+            case 'chess_com':
+                axios.get('https://api.chess.com/pub/player/' + interaction.options.getString('id'))
+                    .then(function (response) {
+                        const mongoClient = new MongoClient(dbConnectionString);
+
+                        (async () => {
+                            try {
+                                const result = await mongoClient.db(mongoDB).collection(mongoCol).findOne({ chesscomID: response.data.username })
+
+                                const embed = new EmbedBuilder()
+                                    .setColor(0x2cee1a)
+                                    .setTitle('Şikayetiniz Hakemlere İletildi')
+                                    .setURL('https://www.chess.com/member/' + response.data.username)
+                                    .setFields(
+                                        { name: 'Kullanıcı', value: `<@${result.discordID}> - ${response.data.username}` },
+                                        { name: 'Hesap Linki', value: 'https://www.chess.com/member/' + response.data.username },
+                                    )
+                                    .setThumbnail('https://cdn.discordapp.com/attachments/1065015635299537028/1066379362414379100/Satranc101Logo_1.png');
+
+                                const message = interaction.reply({
+                                    embeds: [embed],
+                                    ephemeral: true
+                                });
+
+                                await startSession(client, interaction.options.getSubcommand(), `<@${result.discordID}> - ${response.data.username}`, response.data.username, interaction.options.getString('aciklama'));
+                                console.log(`${interaction.user.tag} - ${interaction.user.id} bir oyun bildirdi.`)
+
+                            } catch (error) {
+                                const embed = new EmbedBuilder()
+                                    .setColor(0x2cee1a)
+                                    .setTitle('Şikayetiniz Hakemlere İletildi')
+                                    .setURL('https://www.chess.com/member/' + response.data.username)
+                                    .setFields(
+                                        { name: 'Kullanıcı', value: `${response.data.username}` },
+                                        { name: 'Hesap Linki', value: 'https://www.chess.com/member/' + response.data.username },
+                                    )
+                                    .setThumbnail('https://cdn.discordapp.com/attachments/1065015635299537028/1066379362414379100/Satranc101Logo_1.png');
+
+                                const message = interaction.reply({
+                                    embeds: [embed],
+                                    ephemeral: true
+                                });
+
+                                await startSession(client, interaction.options.getSubcommand(), response.data.username, response.data.username, interaction.options.getString('aciklama'));
+                                console.log(`${interaction.user.tag} - ${interaction.user.id} bir oyun bildirdi.`);
+                            }
+                        })();
+                    })
+                    .catch(error => {
+                        const invalidAccount = new EmbedBuilder()
+                            .setColor(0xec0505)
+                            .setTitle('Hesap Bulunamadı.')
+                            .setDescription(`\`${interaction.options.getString('id')}\` adında bir chess.com hesabı bulunamadı.`)
                             .setThumbnail('https://cdn.discordapp.com/attachments/1065015635299537028/1066379362414379100/Satranc101Logo_1.png');
-    
-                        const message = interaction.reply({
-                            embeds: [embed],
-                            ephemeral: true
-                        });
-    
-                        await startSession(client,`${userName}`, userId, url, interaction.options.getString('açıklama'));
-                        console.log(`${interaction.user.tag} - ${interaction.user.id} bir oyun bildirdi.`)
-                    }
-                }) ();
 
-            })
-            .catch(error => {
-                const invalidAccount = new EmbedBuilder()
-                    .setColor(0xec0505)
-                    .setTitle('Geçersiz ID')
-                    .setDescription(`Geçersiz URL:\nhttps://lichess.org/${gameId}`)
-                    .setThumbnail('https://cdn.discordapp.com/attachments/1065015635299537028/1066379362414379100/Satranc101Logo_1.png');
+                        interaction.reply({ embeds: [invalidAccount] });
+                    });
+                break;
 
-                interaction.reply({
-                    embeds: [invalidAccount],
-                    ephemeral: true
-                });
-            });
+            default:
+                break;
+        }
     }
 }
--- functions/report.js
+++ functions/report.js
@@ -4,25 +4,44 @@ const { default: axios } = require('axios');
 const { Client, EmbedBuilder, Colors, roleMention } = require('discord.js');
 const { reportSessionChannelID, refRoleID } = require('../config.json');
 
-async function startSession(client, userName, userId, url, description) {
+async function startSession(client, platform, userName, userId, description) {
+    if (platform == 'lichess') {
+        const embed = new EmbedBuilder()
+            .setColor(Colors.Grey)
+            .setTitle('İnceleme Oturumu')
+            .setURL(`https://lichess.org/@/${userId}`)
+            .setFields(
+                { name: 'Kullanıcı', value: userName },
+                { name: 'Hesap Linki', value: `https://lichess.org/@/${userId}` },
+            )
+            .setDescription(description)
+            .setThumbnail('https://cdn.discordapp.com/attachments/1065015635299537028/1066379362414379100/Satranc101Logo_1.png');
 
-    const embed = new EmbedBuilder()
-        .setColor(Colors.Grey)
-        .setTitle('İnceleme Oturumu')
-        .setURL(url)
-        .setFields(
-            { name: 'Kullanıcı', value: userName },
-            { name: 'Hesap Linki', value: `https://lichess.org/@/${userId}` },
-            { name: 'Oyun Linki', value: url },
-        )
-        .setDescription(description)
-        .setThumbnail('https://cdn.discordapp.com/attachments/1065015635299537028/1066379362414379100/Satranc101Logo_1.png');
+        const message = await client.channels.cache.get(reportSessionChannelID).send({
+            content: `${roleMention(refRoleID)}`,
+            embeds: [embed],
+            fetchReply: true
+        });
+        message.react('👍');
+        message.react('👎');
+    } else {
+        const embed = new EmbedBuilder()
+            .setColor(Colors.Grey)
+            .setTitle('İnceleme Oturumu')
+            .setURL(`https://www.chess.com/member/${userId}`)
+            .setFields(
+                { name: 'Kullanıcı', value: userName },
+                { name: 'Hesap Linki', value: `https://www.chess.com/member/${userId}` },
+            )
+            .setDescription(description)
+            .setThumbnail('https://cdn.discordapp.com/attachments/1065015635299537028/1066379362414379100/Satranc101Logo_1.png');
 
-    const message = await client.channels.cache.get(reportSessionChannelID).send({
-        content: `${roleMention(refRoleID)}`,
-        embeds: [embed],
-        fetchReply: true
-    });
-    message.react('👍');
-    message.react('👎');
+        const message = await client.channels.cache.get(reportSessionChannelID).send({
+            content: `${roleMention(refRoleID)}`,
+            embeds: [embed],
+            fetchReply: true
+        });
+        message.react('👍');
+        message.react('👎');
+    }
 }
